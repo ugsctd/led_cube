c5972134730dfa5cd7ee2b4a9f31b8ad4f23f917
Ugs <ugsctd@gmail.com>
2019-12-03 22:30:21 +0100

    just had to add the damn binary.h

--- alphabet.h
+++ alphabet.h
@@ -8,17 +8,12 @@ using namespace std;
 typedef array<unsigned char, 8> letterArray;
 typedef std::map<char, letterArray> ledArray;
 
-// std::map<char, letterArray> alphabet;
-
 class AlphabetClass
 {
 public:
     AlphabetClass();
-    // std::map<char, letterArray> alphabet;
-
     ledArray alphabet;
 
-    // std::map<char, letterArray> create_alphabet();
 };
 
 extern AlphabetClass Alphabet;
